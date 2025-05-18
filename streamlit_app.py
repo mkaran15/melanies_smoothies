f4e416d7a41b060bc3a5e099bd0a64ba7e48a77e
Karan Maheshwari <63113271+mkaran15@users.noreply.github.com>
2025-05-18 23:38:29 +0530

    Update streamlit_app.py
    Added fruit name column as well

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ session=cnx.session()
 name_on_order=st.text_input('Name on Smoothie: ')
 st.write("The name on your smoothie will be: ",name_on_order)
 
-my_dataframe=session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('SEARCH_ON'))
+my_dataframe=session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 

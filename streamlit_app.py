426ed363b900f147b3d5424a3bab8947e8e340af
Karan Maheshwari <63113271+mkaran15@users.noreply.github.com>
2025-05-18 22:21:17 +0530

    Update streamlit_app.py
    using JSON data of API call into dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
@@ -51,7 +52,9 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success("""Your Smoothie is ordered, """ + name_on_order + """!""", icon="✅")
 
-import requests
+
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 

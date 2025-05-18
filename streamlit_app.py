8639c6fe0f6e79ef8f2427f058d4b554cd21e7a1
Karan Maheshwari <63113271+mkaran15@users.noreply.github.com>
2025-05-18 22:27:42 +0530

    Update streamlit_app.py
    edited the ingredients list

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,10 @@ if ingredients_list:
     
     for each_fruit in ingredients_list:
         ingredients_string+=each_fruit + ' '
-    #st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        #st.text(smoothiefroot_response.json())
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        #st.write(ingredients_string)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -53,8 +56,6 @@ if time_to_insert:
     st.success("""Your Smoothie is ordered, """ + name_on_order + """!""", icon="✅")
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 
 
